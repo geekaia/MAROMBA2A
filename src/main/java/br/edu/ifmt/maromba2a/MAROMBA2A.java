f9c0d3099bdf0b42921b11f059e16e0cd3f72009
geekaia <geekaia@gmail.com>
2022-10-01 11:59:11 -0300

    MAROMBA2A-01-out-2022

---
+++ src/main/java/br/edu/ifmt/maromba2a/MAROMBA2A.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.edu.ifmt.maromba2a;
+
+/**
+ *
+ * @author geekaia
+ */
+public class MAROMBA2A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá mundo"); 
+    }
+    
+}
